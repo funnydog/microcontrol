2253bd0c25d079a7e281b579d5518148895d7663
funnydog <funnydog@users.noreply.github.com>
2016-11-28 08:38:12 +0100

    Silence the warnings

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
  * main.c
  */
 
+#define _DEFAULT_SOURCE
+
 #include "ch.h"
 #include "hal.h"
 #include "ch_test.h"
+
+#include <stdlib.h>
+#include <string.h>
 
 #include "usbcfg.h"
 
@@ -27,19 +32,6 @@ static THD_FUNCTION(Thread1, arg)
 	}
 }
 
-/* Breathing Sleep LED brighness(PWM On period) table
- *
- * http://www.wolframalpha.com/input/?i=%28sin%28+x%2F64*pi%29**8+*+255%2C+x%3D0+to+63
- * (0..63).each {|x| p ((sin(x/64.0*PI)**8)*255).to_i }
- */
-/* ruby -e "a = ((0..255).map{|x| Math.exp(Math.cos(Math::PI+(2*x*(Math::PI)/255)))-Math.exp(-1) }); m = a.max; a.map\!{|x| (9900*x/m).to_i+1}; p a" */
-
-#define BREATHE_STEP 16 /* ms; = 4000ms/TABLE_SIZE */
-#define TABLE_SIZE 256
-static const uint16_t breathing_table[TABLE_SIZE] = {
-  1, 1, 2, 5, 8, 12, 17, 24, 31, 39, 48, 58, 69, 81, 95, 109, 124, 140, 158, 177, 196, 217, 239, 263, 287, 313, 340, 369, 399, 430, 463, 497, 532, 570, 608, 649, 691, 734, 779, 827, 875, 926, 979, 1033, 1090, 1148, 1209, 1271, 1336, 1403, 1472, 1543, 1617, 1693, 1771, 1852, 1935, 2021, 2109, 2199, 2292, 2387, 2486, 2586, 2689, 2795, 2903, 3014, 3127, 3243, 3362, 3482, 3606, 3731, 3859, 3989, 4122, 4256, 4392, 4531, 4671, 4813, 4957, 5102, 5248, 5396, 5545, 5694, 5845, 5995, 6147, 6298, 6449, 6600, 6751, 6901, 7050, 7198, 7344, 7489, 7632, 7772, 7911, 8046, 8179, 8309, 8435, 8557, 8676, 8790, 8900, 9005, 9105, 9201, 9291, 9375, 9454, 9527, 9593, 9654, 9708, 9756, 9797, 9831, 9859, 9880, 9894, 9900, 9901, 9894, 9880, 9859, 9831, 9797, 9756, 9708, 9654, 9593, 9527, 9454, 9375, 9291, 9201, 9105, 9005, 8900, 8790, 8676, 8557, 8435, 8309, 8179, 8046, 7911, 7772, 7632, 7489, 7344, 7198, 7050, 6901, 6751, 6600, 6449, 6298, 6147, 5995, 5845, 5694, 5545, 5396, 5248, 5102, 4957, 4813, 4671, 4531, 4392, 4256, 4122, 3989, 3859, 3731, 3606, 3482, 3362, 3243, 3127, 3014, 2903, 2795, 2689, 2586, 2486, 2387, 2292, 2199, 2109, 2021, 1935, 1852, 1771, 1693, 1617, 1543, 1472, 1403, 1336, 1271, 1209, 1148, 1090, 1033, 979, 926, 875, 827, 779, 734, 691, 649, 608, 570, 532, 497, 463, 430, 399, 369, 340, 313, 287, 263, 239, 217, 196, 177, 158, 140, 124, 109, 95, 81, 69, 58, 48, 39, 31, 24, 17, 12, 8, 5, 2, 1, 1
-};
-
 static struct pwm_desc
 {
 	ioportid_t port;
@@ -61,24 +53,13 @@ static THD_FUNCTION(BreatheThread, arg)
 	(void)arg;
 	chRegSetThreadName("breather");
 	while(true) {
-/*
-		uint32_t mask = 0;
-		struct pwm_desc *d = pwm_channels;
-		for (unsigned i = 0; i < 3; i++, d++) {
-			FTM0->CHANNEL[d->ch].CnV =
-				(uint32_t)breathing_table[d->pos] * 4096 / 10000;
-			if (++d->pos >= TABLE_SIZE)
-				d->pos = 0;
-			mask |= 1 << d->ch;
-		}
-		FTM0->PWMLOAD = FTM_PWMLOAD_LDOK_MASK;
-*/
-		chThdSleepMilliseconds(BREATHE_STEP);
+		chThdSleepMilliseconds(100);
 	}
 }
 
 #define sduGet(sdup) chnGetTimeout((sdup), TIME_INFINITE)
 #define sduPut(sdup, b) chnPutTimeout((sdup), (b), TIME_INFINITE)
+#define sduWrite(sdup, str) chnWrite((sdup), (uint8_t *)(str), sizeof((str)-1))
 
 static size_t read_line(char *dst, size_t len)
 {
@@ -93,9 +74,7 @@ static size_t read_line(char *dst, size_t len)
 			dst[pos] = c;
 			pos++;
 		} else if (c == 0x7F && pos > 0) {
-			sduPut(&SDU1, '\b');
-			sduPut(&SDU1, ' ');
-			sduPut(&SDU1, '\b');
+			sduWrite(&SDU1, "\b \b");
 			pos--;
 		} else {
 			/*
@@ -104,8 +83,7 @@ static size_t read_line(char *dst, size_t len)
 			 */
 		}
 	}
-	sduPut(&SDU1, '\r');
-	sduPut(&SDU1, '\n');
+	sduWrite(&SDU1, "\r\n");
 	if (len) {
 		dst[pos] = 0;
 	}
@@ -181,7 +159,7 @@ static int getopt(int argc, char * const argv[], const char *optstr)
 	}
 
 	/* find the current option in the optstr */
-	char *d;
+	const char *d;
 	for (d = optstr; *d  && *d != optopt; d++)
 		;
 
@@ -235,7 +213,7 @@ static int setled(int argc, char *argv[])
 		case 'g': mask |= 2; break;
 		case 'b': mask |= 4; break;
 		default:
-			chnWrite(&SDU1, "wrong option\r\n", 14);
+			sduWrite(&SDU1, "wrong option\r\n");
 			return -1;
 		}
 	}
@@ -246,7 +224,7 @@ static int setled(int argc, char *argv[])
 	char *end;
 	uint16_t value = strtoul(argv[optind], &end, 10);
 	if (argv[optind] == end) {
-		chnWrite(&SDU1, "wrong value\r\n", 13);
+		sduWrite(&SDU1, "wrong value\r\n");
 		return -1;
 	}
 
@@ -279,8 +257,8 @@ static int execute_cmd(int argc, char *argv[])
 
 	/* dump the arguments */
 	for(; *argv; argv++) {
-		chnWrite(&SDU1, *argv, strlen(*argv));
-		chnWrite(&SDU1, "*\r\n", 3);
+		chnWrite(&SDU1, (uint8_t *)*argv, strlen(*argv));
+		sduWrite(&SDU1, "*\r\n");
 	}
 	return -1;
 }
@@ -296,7 +274,7 @@ static THD_FUNCTION(shell, arg)
 	static char *argv[10];
 
 	while (true) {
-		chnWrite(&SDU1, "$ ", 2);
+		sduWrite(&SDU1, "$ ");
 
 		read_line(line, sizeof(line));
 		argc = split_args(argv, sizeof(argv) / sizeof(argv[0]), line);
@@ -370,13 +348,15 @@ int main(void) {
 	 */
 	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
-//	test_execute((BaseSequentialStream *)&SD1);
+	/*
+	test_execute((BaseSequentialStream *)&SD1);
+	*/
 	while (true) {
 		chThdSleepMilliseconds(250);
-
 		if (SDU1.config->usbp->state == USB_ACTIVE) {
-			thread_t *shelltp = chThdCreateFromHeap(NULL, 256, "shell", NORMALPRIO+1,
-								shell, NULL);
+			thread_t *shelltp = chThdCreateStatic(
+				waShell, sizeof(waShell), NORMALPRIO+1,
+				shell, NULL);
 			chThdWait(shelltp);
 		}
 	}
